684a360dfcf4e06f1722a5f9050091104a515aa9
Theodoros Deligiannidis <delikikitsa@gmail.com>
2019-07-28 18:44:59 +0300

    First commit

---
+++ src/Rates.php
@@ -0,0 +1,23 @@
+<?php namespace Theodoros\Coinmotion;
+
+/**
+*  @author Theodoros Deligiannidis
+*/
+
+class Rates
+{
+
+  /**
+  * Sample method 
+  *
+  * Always create a corresponding docblock for each method, describing what it is for,
+  * this helps the phpdocumentator to properly generator the documentation
+  *
+  * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
+  *
+  * @return string
+  */
+   public function method1($param1){
+			return "Hello World";
+   }
+}
